f369ddb169483a846a562b3986c334a9900f897a
asasinmode <skin9470@gmail.com>
2024-03-04 20:58:53 +0100

    feat: add logger

--- src/main/java/com/asasinmode/wheelbarrow/Wheelbarrow.java
+++ src/main/java/com/asasinmode/wheelbarrow/Wheelbarrow.java
@@ -3,9 +3,12 @@ package com.asasinmode.wheelbarrow;
 import com.asasinmode.wheelbarrow.item.ModItems;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Wheelbarrow implements ModInitializer {
 	public static final String MOD_ID = "wheelbarrow";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
