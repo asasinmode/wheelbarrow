8d03c25e454cea34d4e9d2b7024cebbe052d4dfb
asasinmode <skin9470@gmail.com>
2024-03-04 20:36:57 +0100

    feat: place on click pos

--- src/main/java/com/asasinmode/wheelbarrow/item/custom/WheelbarrowItem.java
+++ src/main/java/com/asasinmode/wheelbarrow/item/custom/WheelbarrowItem.java
@@ -4,22 +4,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 
-import java.util.Iterator;
-import java.util.List;
-
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.event.GameEvent.Emitter;
 import net.minecraft.item.BoatItem;
+import net.minecraft.item.MinecartItem;
 
 public class WheelbarrowItem extends Item {
 	// should be an oxidation wheelbarrow type
@@ -47,7 +42,7 @@ public class WheelbarrowItem extends Item {
 			ServerWorld serverWorld = (ServerWorld) world;
 
 			AbstractMinecartEntity abstractMinecartEntity = AbstractMinecartEntity.create(serverWorld,
-					(double) hitPos.getX(), (double) blockPos.up().getY(), (double) hitPos.getZ(),
+					(double) hitPos.getX(), (double) hitPos.getY(), (double) hitPos.getZ(),
 					this.type, itemStack, context.getPlayer());
 
 			if (!world.isSpaceEmpty(abstractMinecartEntity, abstractMinecartEntity.getBoundingBox())) {
@@ -57,7 +52,7 @@ public class WheelbarrowItem extends Item {
 			abstractMinecartEntity.setYaw(userYaw);
 			serverWorld.spawnEntity(abstractMinecartEntity);
 			serverWorld.emitGameEvent(GameEvent.ENTITY_PLACE, blockPos,
-					Emitter.of(context.getPlayer(), serverWorld.getBlockState(blockPos.down())));
+					Emitter.of(context.getPlayer(), serverWorld.getBlockState(blockPos)));
 		}
 
 		itemStack.decrement(1);
