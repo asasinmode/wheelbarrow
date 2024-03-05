41fac755848014da12b592ad9c606a3e6c57491e
asasinmode <skin9470@gmail.com>
2024-03-05 19:50:50 +0100

    feat: working model init

--- src/main/java/com/asasinmode/wheelbarrow/entity/ModEntities.java
+++ src/main/java/com/asasinmode/wheelbarrow/entity/ModEntities.java
@@ -1,20 +1,21 @@
 package com.asasinmode.wheelbarrow.entity;
 
 import com.asasinmode.wheelbarrow.Wheelbarrow;
+import com.asasinmode.wheelbarrow.entity.custom.*;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModEntities {
-	public static final Item COPPER_WHEELBARROW = registerEntity("copper_wheelbarrow",
-			new CopperWheelbarrowItem(new FabricItemSettings().maxCount(1)));
-
-	private static Item registerEntity(String name, Item item) {
-		return Registry.register(Registries.ITEM, new Identifier(Wheelbarrow.MOD_ID, name), item);
-	}
+	public static final EntityType<WheelbarrowEntity> WHEELBARROW = Registry.register(Registries.ENTITY_TYPE,
+			new Identifier(Wheelbarrow.MOD_ID, "copper_wheelbarrow"),
+			FabricEntityTypeBuilder.<WheelbarrowEntity>create(SpawnGroup.MISC, WheelbarrowEntity::new)
+					.dimensions(EntityDimensions.fixed(0.001F, 0.001F)).build());
 
 	public static void registerModEntities() {
 	}
---
+++ src/main/java/com/asasinmode/wheelbarrow/entity/custom/WheelbarrowEntity.java
@@ -0,0 +1,76 @@
+package com.asasinmode.wheelbarrow.entity.custom;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+
+public class WheelbarrowEntity extends Entity {
+	private static final TrackedData<Integer> OXIDATION_LEVEL;
+
+	public WheelbarrowEntity(EntityType<? extends WheelbarrowEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	protected void initDataTracker() {
+		this.dataTracker.startTracking(OXIDATION_LEVEL, Type.COPPER.ordinal());
+	}
+
+	static enum Type {
+		COPPER("copper"),
+		EXPOSED("exposed"),
+		WEATHERED("weathered"),
+		OXIDIZED("oxidized");
+
+		private final String type;
+
+		Type(String type) {
+			this.type = type;
+		}
+
+		public String asString() {
+			return type;
+		}
+
+		public static Type getType(String value) {
+			for (Type enumConstant : Type.values()) {
+				if (enumConstant.asString().equals(value)) {
+					return enumConstant;
+				}
+			}
+			throw new IllegalArgumentException("No enum constant with value: " + value);
+		}
+
+		public static Type getType(int ordinal) {
+			if (ordinal >= 0 && ordinal < values().length) {
+				return values()[ordinal];
+			}
+			throw new IllegalArgumentException("No enum constant with ordinal value: " + ordinal);
+		}
+	}
+
+	protected void writeCustomDataToNbt(NbtCompound nbt) {
+		nbt.putString("OxidationLevel", this.getVariant().asString());
+	}
+
+	protected void readCustomDataFromNbt(NbtCompound nbt) {
+		if (nbt.contains("OxidationLevel", 8)) {
+			this.setVariant(Type.getType(nbt.getString("Type")));
+		}
+	}
+
+	public void setVariant(Type type) {
+		this.dataTracker.set(OXIDATION_LEVEL, type.ordinal());
+	}
+
+	public Type getVariant() {
+		return Type.getType((Integer) this.dataTracker.get(OXIDATION_LEVEL));
+	}
+
+	static {
+		OXIDATION_LEVEL = DataTracker.registerData(WheelbarrowEntity.class, TrackedDataHandlerRegistry.INTEGER);
+	}
+}
