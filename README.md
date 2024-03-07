20d3a826b4e884d0d49965829bd4fdbb9b322bbf
asasinmode <skin9470@gmail.com>
2024-03-07 21:04:17 +0100

    feat: init tick & interact

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # wheelbarrow
 
+maybe fill with water and yoink water creatures
+keybind/other thing for yeeting stuff out in front
+
 github action on release publish to
 
 - maven
--- src/main/java/com/asasinmode/wheelbarrow/entity/custom/WheelbarrowEntity.java
+++ src/main/java/com/asasinmode/wheelbarrow/entity/custom/WheelbarrowEntity.java
@@ -1,10 +1,15 @@
 package com.asasinmode.wheelbarrow.entity.custom;
 
+import java.util.Iterator;
+import java.util.List;
+
 import com.asasinmode.wheelbarrow.entity.ModEntities;
 import com.asasinmode.wheelbarrow.item.ModItems;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
@@ -12,15 +17,26 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.MinecartEntity;
 
 public class WheelbarrowEntity extends Entity {
 	private static final TrackedData<Integer> OXIDATION_LEVEL;
+	private int lerpTicks;
+	private float yawVelocity;
+	private float velocityDecay;
+	private double boatYaw;
+	private double boatPitch;
+	private double x;
+	private double y;
+	private double z;
 
 	public WheelbarrowEntity(EntityType<? extends WheelbarrowEntity> entityType, World world) {
 		super(entityType, world);
@@ -99,29 +115,6 @@ public class WheelbarrowEntity extends Entity {
 		return (WheelbarrowEntity) wheelbarrowEntity;
 	}
 
-	public boolean collidesWith(Entity other) {
-		return canCollide(this, other);
-	}
-
-	public static boolean canCollide(Entity entity, Entity other) {
-		return (other.isCollidable() || other.isPushable()) && !entity.isConnectedThroughVehicle(other);
-	}
-
-	public boolean isCollidable() {
-		return true;
-	}
-
-	public boolean isPushable() {
-		return true;
-	}
-
-	// not sure what it does yet
-	public void pushAwayFrom(Entity entity) {
-		if (entity.getBoundingBox().minY <= this.getBoundingBox().minY) {
-			super.pushAwayFrom(entity);
-		}
-	}
-
 	public Item asItem() {
 		Item item;
 
@@ -145,14 +138,47 @@ public class WheelbarrowEntity extends Entity {
 		return item;
 	}
 
+	public ActionResult interact(PlayerEntity player, Hand hand) {
+		if (player.shouldCancelInteraction()) {
+			return ActionResult.PASS;
+		} else if (!this.getWorld().isClient) {
+			return player.startRiding(this) ? ActionResult.CONSUME : ActionResult.PASS;
+		} else {
+			return ActionResult.SUCCESS;
+		}
+	}
+
+	public boolean collidesWith(Entity other) {
+		return canCollide(this, other);
+	}
+
+	public static boolean canCollide(Entity entity, Entity other) {
+		return (other.isCollidable() || other.isPushable()) && !entity.isConnectedThroughVehicle(other);
+	}
+
+	public boolean isCollidable() {
+		return true;
+	}
+
+	public boolean isPushable() {
+		return true;
+	}
+
+	// not sure what it does yet
+	public void pushAwayFrom(Entity entity) {
+		if (entity.getBoundingBox().minY <= this.getBoundingBox().minY) {
+			super.pushAwayFrom(entity);
+		}
+	}
+
 	public boolean canHit() {
 		return !this.isRemoved();
 	}
 
-	// figure out when its used
-	public Direction getMovementDirection() {
-		return this.getHorizontalFacing().rotateYClockwise();
-	}
+	// maybe useful
+	// public Direction getMovementDirection() {
+	// return this.getHorizontalFacing().rotateYClockwise();
+	// }
 
 	protected Text getDefaultName() {
 		return Text.translatable(this.asItem().getTranslationKey());
@@ -160,6 +186,78 @@ public class WheelbarrowEntity extends Entity {
 
 	public ItemStack getPickBlockStack() {
 		return new ItemStack(this.asItem());
+	}
+
+	protected int getMaxPassengers() {
+		return 2;
+	}
+
+	public boolean canBeYoinked(Entity entity) {
+		return entity.getWidth() < this.getWidth();
+	}
+
+	public void tick() {
+		super.tick();
+
+		this.updatePositionAndRotation();
+
+		if (this.isLogicalSideForUpdatingMovement()) {
+			this.updateVelocity();
+			this.move(MovementType.SELF, this.getVelocity());
+		} else {
+			this.setVelocity(Vec3d.ZERO);
+		}
+
+		this.checkBlockCollision();
+
+		List<Entity> list = this.getWorld().getOtherEntities(this, this.getBoundingBox(),
+				EntityPredicates.canBePushedBy(this));
+
+		if (!list.isEmpty()) {
+			boolean canYoink = !this.getWorld().isClient && !(this.getControllingPassenger() instanceof PlayerEntity);
+
+			Iterator<Entity> entitiesIterator = list.iterator();
+			// check what happens without the loop
+			while (true) {
+				Entity entity;
+				do {
+					if (!entitiesIterator.hasNext()) {
+						return;
+					}
+
+					entity = (Entity) entitiesIterator.next();
+				} while (entity.hasPassenger(this));
+
+				if (canYoink && this.getPassengerList().size() < this.getMaxPassengers() && !entity.hasVehicle()
+						&& this.canBeYoinked(entity) && entity instanceof LivingEntity
+						&& !(entity instanceof PlayerEntity)) {
+					// entity.startRiding(this);
+				} else {
+					this.pushAwayFrom(entity);
+				}
+			}
+		}
+	}
+
+	private void updatePositionAndRotation() {
+		if (this.isLogicalSideForUpdatingMovement()) {
+			this.lerpTicks = 0;
+			this.updateTrackedPosition(this.getX(), this.getY(), this.getZ());
+		}
+
+		if (this.lerpTicks > 0) {
+			this.lerpPosAndRotation(this.lerpTicks, this.x, this.y, this.z, this.boatYaw, this.boatPitch);
+			--this.lerpTicks;
+		}
+	}
+
+	private void updateVelocity() {
+		this.velocityDecay = 0.9f;
+
+		Vec3d vec3d = this.getVelocity();
+
+		this.setVelocity(vec3d.x * (double) this.velocityDecay, vec3d.y, vec3d.z * (double) this.velocityDecay);
+		this.yawVelocity *= this.velocityDecay;
 	}
 
 	static {
