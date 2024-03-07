2968ddb7f038750d7e42c096730f27cdb5bd64d1
asasinmode <skin9470@gmail.com>
2024-03-07 18:12:32 +0100

    feat: update model

--- src/client/java/com/asasinmode/wheelbarrow/model/WheelbarrowEntityModel.java
+++ src/client/java/com/asasinmode/wheelbarrow/model/WheelbarrowEntityModel.java
@@ -15,6 +15,9 @@ import net.minecraft.client.util.math.MatrixStack;
 
 public class WheelbarrowEntityModel extends EntityModel<WheelbarrowEntity> {
 	private final ModelPart front;
+	// private final ModelPart front_right;
+	// private final ModelPart front_left;
+	// private final ModelPart front_front;
 	private final ModelPart back;
 	// private final ModelPart handles;
 	private final ModelPart left;
@@ -32,33 +35,50 @@ public class WheelbarrowEntityModel extends EntityModel<WheelbarrowEntity> {
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData front = modelPartData.addChild("front",
-				ModelPartBuilder.create().uv(36, 36).cuboid(-6.0F, 6.0F, -14.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
-						.uv(36, 28).cuboid(-6.0F, 8.0F, -16.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
-						.uv(36, 32).cuboid(-6.0F, 4.0F, -12.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
-						.uv(0, 48).cuboid(-6.0F, 2.0F, -10.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F)),
+		ModelPartData front = modelPartData.addChild("front", ModelPartBuilder.create(),
+				ModelTransform.pivot(0.0F, 6.0F, 8.0F));
+
+		ModelPartData front_right = front.addChild("front_right",
+				ModelPartBuilder.create().uv(18, 24).cuboid(-1.0F, -4.0F, 0.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F))
+						.uv(18, 18).cuboid(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 4.0F, new Dilation(0.0F))
+						.uv(0, 8).cuboid(-1.0F, 0.0F, 0.0F, 2.0F, 2.0F, 6.0F, new Dilation(0.0F))
+						.uv(48, 0).cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(7.0F, 0.0F, 0.0F));
+
+		ModelPartData front_left = front.addChild("front_left",
+				ModelPartBuilder.create().uv(0, 24).cuboid(-1.0F, -4.0F, 0.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F))
+						.uv(0, 18).cuboid(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 4.0F, new Dilation(0.0F))
+						.uv(0, 0).cuboid(-1.0F, 0.0F, 0.0F, 2.0F, 2.0F, 6.0F, new Dilation(0.0F))
+						.uv(42, 40).cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(-7.0F, 0.0F, 0.0F));
+
+		ModelPartData front_front = front.addChild("front_front",
+				ModelPartBuilder.create().uv(0, 48).cuboid(-6.0F, 2.0F, 6.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
+						.uv(36, 36).cuboid(-6.0F, 0.0F, 4.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
+						.uv(36, 32).cuboid(-6.0F, -2.0F, 2.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F))
+						.uv(36, 28).cuboid(-6.0F, -4.0F, 0.0F, 12.0F, 2.0F, 2.0F, new Dilation(0.0F)),
 				ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
 		ModelPartData back = modelPartData.addChild("back",
-				ModelPartBuilder.create().uv(36, 18).cuboid(-12.0F, 0.0F, -3.0F, 16.0F, 8.0F, 2.0F, new Dilation(0.0F)),
-				ModelTransform.pivot(4.0F, 2.0F, 9.0F));
+				ModelPartBuilder.create().uv(36, 18).cuboid(-8.0F, -4.0F, -1.0F, 16.0F, 8.0F, 2.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(0.0F, 6.0F, -7.0F));
 
 		ModelPartData handles = back.addChild("handles",
-				ModelPartBuilder.create().uv(48, 0).cuboid(6.0F, 8.0F, 10.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F))
-						.uv(42, 40).cuboid(-8.0F, 8.0F, 10.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F)),
-				ModelTransform.pivot(-4.0F, -2.0F, -11.0F));
+				ModelPartBuilder.create().uv(32, 50).cuboid(6.0F, -1.0F, -8.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F))
+						.uv(20, 48).cuboid(-8.0F, -1.0F, -8.0F, 2.0F, 2.0F, 8.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(0.0F, 3.0F, -1.0F));
 
 		ModelPartData left = modelPartData.addChild("left",
-				ModelPartBuilder.create().uv(0, 18).cuboid(-8.0F, 2.0F, -8.0F, 2.0F, 8.0F, 14.0F, new Dilation(0.0F)),
-				ModelTransform.pivot(14.0F, 0.0F, 0.0F));
+				ModelPartBuilder.create().uv(0, 18).cuboid(-1.0F, -4.0F, -7.0F, 2.0F, 8.0F, 14.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(7.0F, 6.0F, 1.0F));
 
 		ModelPartData right = modelPartData.addChild("right",
-				ModelPartBuilder.create().uv(18, 26).cuboid(-8.0F, 2.0F, -8.0F, 2.0F, 8.0F, 14.0F, new Dilation(0.0F)),
-				ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+				ModelPartBuilder.create().uv(18, 26).cuboid(-1.0F, -4.0F, -7.0F, 2.0F, 8.0F, 14.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(-7.0F, 6.0F, 1.0F));
 
 		ModelPartData bottom = modelPartData.addChild("bottom",
-				ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, 0.0F, -8.0F, 16.0F, 2.0F, 16.0F, new Dilation(0.0F)),
-				ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+				ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, -1.0F, -8.0F, 16.0F, 2.0F, 16.0F, new Dilation(0.0F)),
+				ModelTransform.pivot(0.0F, 1.0F, 0.0F));
 		return TexturedModelData.of(modelData, 128, 128);
 	}
 
