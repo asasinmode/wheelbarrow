4ee37a130284ebb7cc55281313495393a5fe1545
asasinmode <skin9470@gmail.com>
2024-03-19 18:20:42 +0100

    feat(entity): target expression with ModifyExpressionValue

--- src/client/java/com/asasinmode/wheelbarrow/mixin/client/BipedEntityModelMixin.java
+++ src/client/java/com/asasinmode/wheelbarrow/mixin/client/BipedEntityModelMixin.java
@@ -1,13 +1,13 @@
 package com.asasinmode.wheelbarrow.mixin.client;
 
 import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.asasinmode.wheelbarrow.entity.custom.WheelbarrowEntity;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.AnimalModel;
@@ -18,7 +18,6 @@ import net.minecraft.client.render.entity.model.ModelWithHead;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Arm;
 
 @Mixin(BipedEntityModel.class)
@@ -40,24 +39,16 @@ public abstract class BipedEntityModelMixin<T extends LivingEntity, M extends En
 	@Shadow
 	public abstract void setArmAngle(Arm arm, MatrixStack matrices);
 
-	@Inject(method = "Lnet/minecraft/client/render/entity/model/BipedEntityModel;setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V", at = @At("HEAD"))
-	public void setAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks,
-			float netHeadYaw, float headPitch, CallbackInfo ci) {
-		BipedEntityModel<PlayerEntity> thisObject = (BipedEntityModel<PlayerEntity>) (Object) this;
+	@ModifyExpressionValue(method = "Lnet/minecraft/client/render/entity/model/BipedEntityModel;setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/entity/model/BipedEntityModel;riding:Z", opcode = Opcodes.GETFIELD))
+	private boolean modifyRiding(boolean original, T entity, float limbSwing, float limbSwingAmount, float ageInTicks,
+			float netHeadYaw, float headPitch) {
 		Entity vehicle = entity.getVehicle();
+
 		if (vehicle instanceof WheelbarrowEntity wheelbarrow) {
 			Entity controllingPassenger = wheelbarrow.getControllingPassenger();
-			thisObject.riding = thisObject.riding && entity != controllingPassenger;
+			return original && entity != controllingPassenger;
 		}
-	}
 
-	// todo figure out how to target expression/this.riding field access
-	// @ModifyExpressionValue(method =
-	// "Lnet/minecraft/client/render/entity/model/BipedEntityModel;setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V",
-	// at = @At(value = "FIELD", target =
-	// "Lnet/minecraft/client/render/entity/model/EntityModel;riding:Z", opcode =
-	// Opcodes.GETFIELD))
-	// private boolean modifyRiding(boolean original) {
-	// return original;
-	// }
+		return original;
+	}
 }
