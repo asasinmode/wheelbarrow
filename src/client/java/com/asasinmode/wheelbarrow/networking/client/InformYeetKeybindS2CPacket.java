c1f8a77b4d4891b981cb2f3d1d5f81f098487af6
asasinmode <skin9470@gmail.com>
2024-04-04 11:05:35 +0200

    feat(keybinds): eject message get bound key

--- src/client/java/com/asasinmode/wheelbarrow/networking/client/InformYeetKeybindS2CPacket.java
+++ src/client/java/com/asasinmode/wheelbarrow/networking/client/InformYeetKeybindS2CPacket.java
@@ -1,15 +1,18 @@
 package com.asasinmode.wheelbarrow.networking.client;
 
+import com.asasinmode.wheelbarrow.Keybinds;
+import com.asasinmode.wheelbarrow.Wheelbarrow;
+
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
 
 public class InformYeetKeybindS2CPacket {
 	public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf,
 			PacketSender responseSender) {
-		System.out.println("got inform packet");
-		// ((PlayerEntity) controllingPassenger).sendMessage(Text.translatable("key." +
-		// Wheelbarrow.MOD_ID + ".yeetTooltip", "Z"), true);
+		client.player.sendMessage(Text.translatable("key." +
+				Wheelbarrow.MOD_ID + ".yeetTooltip", Keybinds.yeet.getBoundKeyLocalizedText()), true);
 	}
 }
